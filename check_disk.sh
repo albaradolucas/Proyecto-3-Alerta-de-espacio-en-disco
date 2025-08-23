256d3025de6b7003558da69cf1a8c9f85c308753
Lucas <77122757+albaradolucas@users.noreply.github.com>
2025-08-22 21:55:07 -0300

    Update check_disk.sh

--- check_disk.sh
+++ check_disk.sh
@@ -14,26 +14,34 @@
 
 umbral=$1
 logdir=/var/log/disk_alerts.log
+exit_code=0
 
-if [ -z $umbral ]; then
+if [ -z "$umbral" ]; then
         echo "Debes ingresar un valor de umbral"
-        exit 1
+        exit 2
 fi
 
-if [[ $umbral =~ ^-?[0-9]+$ ]]; then
-        dffs=$(df -P -T| grep -v -E 'tmpfs|devtmpfs|proc|sysfs|debugfs|overlay|squashfs|9p|rootfs'|awk '$6 !~ "^/mnt/" && $6 !~ "^/usr/lib/wsl" && $6 != "/init"'|awk 'NR > 1 {print $1 ":" $6 ":" $7}'|tr -d "%")
-        for line in $dffs; do
-                fs=$(echo $line | cut -d: -f1)
-                use=$(echo $line | cut -d: -f2)
-                mnt=$(echo $line | cut -d: -f3)
-                frmdate=$(date '+%F %T')
 
-                if [ "$use" -le "$umbral" ]; then
-                        echo "$frmdate | $mnt | $use% | $fs | ESTADO: OK"
-                        status 0
-                else
-                        echo "$frmdate | $mnt | $use% | $fs | ESTADO: ALERTADO"
-                        status 2
-                fi
-        done
+if [[ "$umbral" =~ ^-?[0-9]+$ ]]; then
+        if [ "$umbral" -gt 0 ] && [ "$umbral" -lt 100 ]; then
+                dffs=$(df -P -T| grep -v -E 'tmpfs|devtmpfs|proc|sysfs|debugfs|overlay|squashfs|9p|rootfs'|awk '$6 !~ "^/mnt/" && $6 !~ "^/usr/lib/wsl" && $6 != "/init" && $7 !~ "^/snap/"'|awk 'NR > 1 {print $1 ":" $6 ":" $7}'|tr -d "%")
+                for line in $dffs; do
+                        fs=$(echo $line | cut -d: -f1)
+                        use=$(echo $line | cut -d: -f2)
+                        mnt=$(echo $line | cut -d: -f3)
+                        frmdate=$(date '+%F %T')
+
+                        if [ "$use" -le "$umbral" ]; then
+                                echo "$frmdate | $mnt | $use% | $fs | ESTADO: OK"
+                        else
+                                echo "$frmdate | $mnt | $use% | $fs | ESTADO: ALERTADO"
+                        fi
+                done
+        else
+                echo "Debes ingresar un valor entre 0 y 100"
+                exit 2
+        fi
+else
+        echo "Debes ingresar un valor número entero"
+        exit 2
 fi
